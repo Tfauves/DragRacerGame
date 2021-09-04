a6940a511f7ce3f6f33c47e8c25994b95230ce59
Travis Fauvelle <tfauvelle@gmail.com>
2021-09-04 16:13:13 -0400

    cleaned up souts and refactored menu display.

--- src/com/company/dragracer/Main.java
+++ src/com/company/dragracer/Main.java
@@ -8,25 +8,8 @@ import com.company.dragracer.vehicles.Car;
 public class Main {
 
     public static void main(String[] args) {
-//        CarEngine engin1 = new CarEngine(205, "v6 gas", false);
-//        System.out.println(engin1);
-//
-//        Car racer01 = new Car(engin1,155, 0, 0,0,0,"custom racer");
-//        System.out.println(racer01);
-//        racer01.start();
-//        racer01.turnOff();
-//
-//        racer01.coast();
-//        racer01.brake();
-//        racer01.accelerate();
-//        racer01.accelerate();
-//
-//        racer01.stopVehicle();
-//        racer01.accelerate();
 
         Garage garage = new Garage();
-//        garage.listBuild();
-//        garage.carBuilder();
         garage.start();
 
 
