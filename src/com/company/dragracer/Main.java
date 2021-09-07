76f224815690a3ad22344abdadc36ebe865e53db
Travis Fauvelle <tfauvelle@gmail.com>
2021-09-06 21:03:56 -0400

    add welcome to display.

--- src/com/company/dragracer/Main.java
+++ src/com/company/dragracer/Main.java
@@ -2,18 +2,18 @@ package com.company.dragracer;
 
 import com.company.dragracer.enviroments.Garage;
 import com.company.dragracer.enviroments.Speedway;
-import com.company.dragracer.parts.CarEngine;
-import com.company.dragracer.util.UI;
-import com.company.dragracer.vehicles.Car;
+import com.company.dragracer.util.Display;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Display display = new Display();
         Garage garage = new Garage();
         Speedway speedway = new Speedway();
+        display.titleScreen();
         garage.selectCar();
         speedway.selectTrack();
         speedway.race(garage.getPlayerCar());
+
     }
 }
---
+++ src/com/company/dragracer/util/Display.java
@@ -0,0 +1,16 @@
+package com.company.dragracer.util;
+
+public class Display {
+    private static final String WELCOME_PROMPT = "Welcome To Drag Racer";
+
+    public void displayWelcome(String prompt) {
+        System.out.println(prompt);
+    }
+
+    public void titleScreen() {
+        displayWelcome(WELCOME_PROMPT);
+    }
+
+
+
+}
